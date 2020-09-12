8f4b581d36d650cfa893b42d7eb707aff7ac2083
Mimina <yasmin.hamdy@gmail.com>
2020-09-12 05:01:08 +0200

    All basic functionalities working.

--- branches.js
+++ branches.js
@@ -1,37 +1,42 @@
 var branches = [
   {
-    name: 'Nadi Heliopolis',
-    hours: '9AM to 5PM',
-    location: { lat: 30.04573, lng: 31.19527 },
-    address: 'Lorem epsum address',
-    number: '0123456789',
+    name: 'Heliopolis Sporting Club',
+    hours: '5AM to 11:45PM',
+    coordinates: { lat: 30.088984, lng: 31.317309 },
+    address: '17 El-Marghany, Heliopolis, Cairo.',
+    number: '+20224170061',
+    distance: ""
   },
   {
-    name: 'Nadi Al-Gezira',
-    hours: '9AM to 5PM',
-    location: { lat: 30.05106, lng: 31.19352 },
-    address: 'Lorem epsum address',
-    number: '0123456789',
+    name: 'Gezira Sporting Club',
+    hours: '9AM to 1AM',
+    coordinates: { lat: 30.052666, lng: 31.223163 },
+    address: '15 Saray El Gezirah, Zamalek, Cairo.',
+    number: '+20227356000',
+    distance: ""
   },
   {
-    name: 'Nadi Al-Shams',
-    hours: '9AM to 5PM',
-    location: { lat: 30.05106, lng: 31.19352 },
-    address: 'Lorem epsum address',
-    number: '0123456789',
+    name: 'El Shams Sporting Club',
+    hours: '6AM to 1AM',
+    coordinates: { lat: 30.116214, lng: 31.345354 },
+    address: 'Abd El-Hameed Badawi, El Nozha, Cairo.',
+    number: '+20221802460',
+    distance: ""
   },
   {
-    name: 'Nadi Al-Ahly',
-    hours: '9AM to 5PM',
-    location: { lat: 30.05106, lng: 31.19352 },
-    address: 'Lorem epsum address',
-    number: '0123456789',
+    name: 'Al Ahly Club Nasr City',
+    hours: '6AM to 11PM',
+    coordinates: { lat: 30.070369, lng: 31.355282 },
+    address: 'Hassan Mamoon, Nasr City, Cairo.',
+    number: '+201111970758',
+    distance: ""
   },
   {
-    name: 'Nadi El-Saeed',
-    hours: '9AM to 5PM',
-    location: { lat: 30.05106, lng: 31.19352 },
-    address: 'Lorem epsum address',
-    number: '0123456789',
+    name: 'Shooting Club Dokki',
+    hours: '5AM to 12AM',
+    coordinates: { lat: 30.045612, lng: 31.204183 },
+    address: 'Dr Michel Bakhoum, Dokki, Giza.',
+    number: '+20233374559',
+    distance: ""
   },
 ];
--- index.js
+++ index.js
@@ -29,9 +29,15 @@ function toggleDropdown() {
   $('.dropdown-items-ul').slideToggle('show-dropdown');
 }
 
-// Handling Map Interactions ***************************************
+/* map: for initiating a google map.
+   infoWindow: display location popup in map.
+   currentLocation: current location coordinates;
+   service: 
+   pyrmont:
+*/
+var map, infoWindow, service, currentLocation, pyrmont;
 
-var map, infoWindow;
+// Initiate Google Map --------------------------------------------------
 
 function initMap() {
   // zoom levels: 1 (World), 5 (Landmass/continent), 10 (City), 15 (Streets), 20 (Buildings).
@@ -48,6 +54,8 @@ function initMap() {
   markBranches();
 }
 
+// Google Geolocation API -------------------------------------------------
+
 function getCurrentLocation() {
   // called once location-btn is clicked (sets Map to current location).
 
@@ -55,18 +63,23 @@ function getCurrentLocation() {
     // Browser supports geolocatin (get current location).
     navigator.geolocation.getCurrentPosition(
       function (position) {
-        var pos = {
+        currentLocation = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
 
-        infoWindow.setPosition(pos);
+        pyrmont = new google.maps.LatLng(
+          position.coords.latitude,
+          position.coords.longitude
+        );
+
+        infoWindow.setPosition(currentLocation);
         infoWindow.setContent('You are here.');
         infoWindow.open(map);
         map.setZoom(15);
-        map.setCenter(pos);
+        map.setCenter(currentLocation);
 
-        getCurrentAddress(map, infoWindow, pos);
+        getCurrentAddress(map, infoWindow, currentLocation);
       },
       function () {
         handleLocationError(true, infoWindow, map.getCenter());
@@ -78,8 +91,12 @@ function getCurrentLocation() {
   }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
+function handleLocationError(
+  browserHasGeolocation,
+  infoWindow,
+  currentLocation
+) {
+  infoWindow.setPosition(currentLocation);
   infoWindow.setContent(
     browserHasGeolocation
       ? // ? 'Error: The Geolocation service failed.'
@@ -89,19 +106,21 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 }
 
-function getCurrentAddress(map, infowindow, pos) {
-  // get current Address from coordinates (pos) using Geocoder API.
+// Google Geocoder API (NOT WORKING due to Billing) -------------------------------------------------
+
+function getCurrentAddress(map, infowindow) {
+  // get current Address from coordinates (currentLocation).
 
   var geocoder = new google.maps.Geocoder();
 
   if (geocoder) {
-    geocoder.geocode({ latLng: pos }, function (results, status) {
+    geocoder.geocode({ latLng: currentLocation }, function (results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         console.log(results[0].formatted_address);
 
-        // create marker at current position.
+        // create marker at current location.
         const marker = new google.maps.Marker({
-          position: pos,
+          position: currentLocation,
           map: map,
         });
 
@@ -109,10 +128,10 @@ function getCurrentAddress(map, infowindow, pos) {
         infowindow.setContent(results[0].formatted_address);
         infowindow.open(map, marker);
       } else {
-        $('.location-input').css('font-size', '12px');
+        $('.location-input').css('font-size', '11px');
         $('.location-input').attr(
           'placeholder',
-          "Couldn't get current addres due to Billing."
+          'Your current address is displayed on the map.'
         );
 
         console.log('Geocoding failed: ' + status);
@@ -121,7 +140,8 @@ function getCurrentAddress(map, infowindow, pos) {
   }
 }
 
-// create MARKERS & add them to the Map ********************************
+// Create MARKERS & add them to the Map --------------------------------------
+
 function markBranches() {
   branches.forEach(function (branch) {
     createAMarker(branch, map);
@@ -131,7 +151,7 @@ function markBranches() {
 function createAMarker(branchInfo, map) {
   // Create a marker and set its position.
   var marker = new google.maps.Marker({
-    position: branchInfo.location,
+    position: branchInfo.coordinates,
     map: map,
     title: branchInfo.name,
   });
@@ -145,19 +165,73 @@ function createAMarker(branchInfo, map) {
 // to-do: implement this function!!!
 function showStoreInfo(branchInfo) {}
 
-// show Branch Details ********************************
+// Locate Nearest Branch ------------------------------------------------------
 
-function showBranches(branches) {
-  let i = 1;
+function locateNearestBranch() {
+  if (currentLocation == null) {
+    // get current location, if we don't have it.
+    getCurrentLocation();
+    return;
+  }
+
+  // set distances between current lcoation & branches.
+  setBranchesDistance(currentLocation, branches);
+
+  let nearestBranch = getNearestBranch(branches);
+
+  clearBranchesDetailsDiv();
+  showBranchDetail(nearestBranch);
+
+  drawStraightLine(currentLocation, nearestBranch.coordinates);
+
+  // var pyrmont = new google.maps.LatLng(
+  //   currentLocation.lat,
+  //   currentLocation.lan
+  // );
+  // var request = {
+  //   location: pyrmont,
+  //   radius: '5000',
+  //   type: ['restaurant'],
+  // };
+  // service = new google.maps.places.PlacesService(map);
+  // service.nearbySearch(request, callback);
+}
+
+// function callback(results, status) {
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       createMarker(results[i]);
+//     }
+//   }
+// }
+
+// Get Nearest Branch -------------------------------------------------------------
+function getNearestBranch(branches) {
+  let nearestBranch;
+  let i = branches[0].distance;
+
   for (branch of branches) {
-    $('.branches-details-div').append(`
-    <div class="branch-details-div">
-      <a class="address-title">${branch.name}</a>
-      <a class="opening-hours">${branch.hours}</a>
-      <a class="address"><i class="fas fa-directions"></i>${branch.address}</a>
-      <a class="phone"><i class="fa fas-phone-alt"></i>${branch.number}</a>
-    </div>`);
+    if (branch.distance <= i) {
+      nearestBranch = branch;
+    }
+  }
+  return nearestBranch;
+}
 
+// Clear Branches Details Div -----------------------------------------------------
+function clearBranchesDetailsDiv() {
+  $('.branches-details-div').html('');
+}
+
+// Show Branches Details Div ------------------------------------------------------
+
+function showAllBranches() {
+  clearBranchesDetailsDiv();
+
+  let i = 1;
+
+  for (branch of branches) {
+    showBranchDetail(branch);
     if (i < branches.length) {
       $('.branches-details-div').append('<hr>');
     }
@@ -165,18 +239,159 @@ function showBranches(branches) {
   }
 }
 
-function showAllBranches() {
-  if ($('.branches-details-div').css('display') == 'none') {
-    showBranches(branches);
-    $('.branches-details-div').css('display', 'flex');
+// Create Branch Details HTML ------------------------------------------------------
+
+function showBranchDetail(branch) {
+  $('.branches-details-div').append(`
+    <div class="branch-details-div">
+      <a class="branch-title">${branch.name}</a>
+      <a class="opening-hours">${branch.hours}</a>
+      <a class="address"><i class="fa fa-directions"></i> ${branch.address}</a>
+      <a class="phone"><i class="fa fa-phone-alt"></i> ${branch.number}</a>
+    </div>`);
+}
+
+// Google Place Autocomplete (NOT WORKING due to Billing) ------------------------------------------------------
+
+function triggerPlacesAutoComplete() {
+  const placeAutoCompleteInput = document.getElementById('location-input');
+  const searchBox = new google.maps.places.SearchBox(placeAutoCompleteInput);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', () => {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  let markers = [];
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach((place) => {
+      if (!place.geometry) {
+        console.log('Returned place contains no geometry');
+        return;
+      }
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+// Get Distance (in km) between two coordinates -----------------------------------
+
+function haversine_distance(mk1, mk2) {
+  var R = 6371.071; // Radius of the Earth in kilometers
+  var rlat1 = mk1.lat * (Math.PI / 180); // Convert degrees to radians
+  var rlat2 = mk2.lat * (Math.PI / 180); // Convert degrees to radians
+  var difflat = rlat2 - rlat1; // Radian difference (latitudes)
+  var difflon = (mk2.lng - mk1.lng) * (Math.PI / 180); // Radian difference (longitudes)
+
+  var d =
+    2 *
+    R *
+    Math.asin(
+      Math.sqrt(
+        Math.sin(difflat / 2) * Math.sin(difflat / 2) +
+          Math.cos(rlat1) *
+            Math.cos(rlat2) *
+            Math.sin(difflon / 2) *
+            Math.sin(difflon / 2)
+      )
+    );
+  return d;
+}
+
+function setBranchesDistance(currentLoc, branchesJSON) {
+  for (branch of branchesJSON) {
+    branch.distance = haversine_distance(currentLoc, branch.coordinates);
   }
 }
 
-// todo: calculate nearest branches using current location & branch locations!
-function locateNearestBranch() {
-  $('.branches-details-div').css('display', 'none');
-  $('.branches-details-div').html('');
+// Draw a straight line between currentLocation & destination -----------------------------------
 
-  // change to display nearest branches only!
-  showBranches(branches);
+function drawStraightLine(currentLocation, destination) {
+  var line = new google.maps.Polyline({
+    path: [currentLocation, destination],
+    map: map,
+  });
+}
+
+// Google Directions API (NOT WORKING due to Billing) ------------------------------------------------------
+
+function getDirections(currentLocation) {
+  if (currentLocation != null) {
+    let destination = { lat: 30.045612, lng: 31.204183 };
+
+    let directionsService = new google.maps.DirectionsService();
+    let directionsRenderer = new google.maps.DirectionsRenderer();
+
+    directionsRenderer.setMap(map); // Existing map object displays directions
+
+    // Create route from existing points used for markers
+    const route = {
+      origin: currentLocation,
+      destination: destination,
+      travelMode: 'DRIVING',
+    };
+
+    directionsService.route(route, function (response, status) {
+      // anonymous function to capture directions
+      if (status !== 'OK') {
+        console.log('Directions request failed due to ' + status);
+
+        drawStraightLine(currentLocation, destination);
+        return;
+      } else {
+        directionsRenderer.setDirections(response); // Add route to the map
+        var directionsData = response.routes[0].legs[0]; // Get data about the mapped route
+        if (!directionsData) {
+          console.log('Directions request failed');
+          return;
+        } else {
+          console.log(
+            ' Driving distance is ' +
+              directionsData.distance.text +
+              ' (' +
+              directionsData.duration.text +
+              ').'
+          );
+        }
+      }
+    });
+  }
 }
