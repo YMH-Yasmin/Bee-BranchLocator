b7ac1e264d12785fced4d229aeaa489a522d8a24
Yasmin <Yasmin@YASMIN-WORK>
2022-01-16 00:38:52 +0200

    last updates before final delivery

--- node/app.js
+++ node/app.js
@@ -1,68 +1,7 @@
-var outlets,
-  currentLocation,
-  nearestOutlets = [];
+// to remove before commit: 1) API KEY, 2) outlets JSON URL.
 
-outlets = require('./outlets.json');
-
-// ********** FUNCTIONALITY 3&4 - Get & Set Outlet distances.
-function setOutletDistanceInJSON(currentLoc, outletsJSON) {
-  for (outlet of outletsJSON) {
-    outlet.distance = haversine_distance(
-      currentLoc,
-      parseFloat(outlet.Latitude),
-      parseFloat(outlet.Longitude)
-    );
-  }
-}
-
-// Calculate Distance (in km) between two coordinates.
-function haversine_distance(mk1, mk2Lat, mk2lng) {
-  var R = 6371.071; // Radius of the Earth in kilometers
-  var rlat1 = mk1.lat * (Math.PI / 180); // Convert degrees to radians
-  var rlat2 = mk2Lat * (Math.PI / 180); // Convert degrees to radians
-  var difflat = rlat2 - rlat1; // Radian difference (latitudes)
-  var difflon = (mk2lng - mk1.lng) * (Math.PI / 180); // Radian difference (longitudes)
-  var d =
-    2 *
-    R *
-    Math.asin(
-      Math.sqrt(
-        Math.sin(difflat / 2) * Math.sin(difflat / 2) +
-          Math.cos(rlat1) *
-            Math.cos(rlat2) *
-            Math.sin(difflon / 2) *
-            Math.sin(difflon / 2)
-      )
-    );
-  return d;
-}
-// ********** END OF FUNCTIONALITY 3&4.
-
-function setNearestBranches(outletsJSON) {
-  // .slice(0) to pass array by value, leaving branches unaltered.
-  let tempOutlets = outletsJSON.slice(0);
-
-  let d = tempOutlets[0].distance;
-  let closestOutlet;
-
-  nearestOutlets = [];
-
-  for (let i = 0; i < 5; i++) {
-    for (outlet of tempOutlets) {
-      if (outlet.distance <= d) {
-        closestOutlet = outlet;
-      }
-    }
-    tempOutlets.splice(tempOutlets.indexOf(closestOutlet), 1);
-    nearestOutlets.push(closestOutlet);
-    d = tempOutlets[0].distance;
-  }
-}
-
-// ------------------------------------------------------
-
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 
 // const bodyParser = require('body-parser');
 const app = express();
@@ -73,29 +12,103 @@ app.use(express.urlencoded({ extended: false }));
 
 const server = http.createServer(app);
 
-app.use('/getNearestOutlets', (req, res) => {
-  currentLocation = req.body;
-  
-  console.log('Req.body: ' + JSON.stringify(req.body));
-  console.log(currentLocation);
+var outlets;
 
-  setOutletDistanceInJSON(currentLocation, outlets);
+const axios = require("axios");
+// const outlets = require("./outlets(clubs).json");
 
-  setNearestBranches(outlets);
+const getOutlets = async () => {
+    try {
+        return await axios.get("https://cloudFunctionURL.json");
+    } catch (error) {
+        console.log("Failed to fetch outlets.");
+        console.error(error);
+    }
+};
 
-  res.statusCode = 200;
-  res.set('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Content-Type', 'application/json; charset=utf-8'); 
-  res.send(nearestOutlets);
-  res.end();
+const setOutlets = async () => {
+    outlets = await getOutlets();
+    console.log(`Got ${Object.entries(outlets.data).length} outlet entries.`);
+    console.log(`Got ${Object.keys(outlets.data).length} outlet keys.`);
+};
+
+setOutlets();
+
+app.use("/getNearestOutlets", (req, res) => {
+    res.set("Access-Control-Allow-Origin", "*");
+
+    if (!req.body) {
+        res.status(400).send("The server could not process the request.");
+        return;
+    }
+
+    // currentLocation = req.body;
+    var nearestOutlets = [];
+
+    if (!outlets) {
+        setTimeout(() => {
+            setOutletDistanceInJSON(req.body, outlets.data);
+            nearestOutlets = getNearestBranches(outlets.data);
+            res.status(200).send(nearestOutlets).end();
+        }, 5000);
+    } else {
+        setOutletDistanceInJSON(req.body, outlets.data);
+        nearestOutlets = getNearestBranches(outlets.data);
+        res.status(200).send(nearestOutlets).end();
+    }
 });
+
+// ********** FUNCTIONALITY 3&4 - Get & Set Outlet distances.
+function setOutletDistanceInJSON(currentLoc, outletsJSON) {
+    for (outlet of outletsJSON) {
+        outlet.distance = haversine_distance(
+            currentLoc,
+            parseFloat(outlet.Latitude),
+            parseFloat(outlet.Longitude)
+        );
+    }
+}
+
+// Calculate Distance (in km) between two coordinates.
+function haversine_distance(mk1, mk2Lat, mk2lng) {
+    var R = 6371.071; // Radius of the Earth in kilometers
+    var rlat1 = mk1.lat * (Math.PI / 180); // Convert degrees to radians
+    var rlat2 = mk2Lat * (Math.PI / 180); // Convert degrees to radians
+    var difflat = rlat2 - rlat1; // Radian difference (latitudes)
+    var difflon = (mk2lng - mk1.lng) * (Math.PI / 180); // Radian difference (longitudes)
+    var d =
+        2 *
+        R *
+        Math.asin(
+            Math.sqrt(
+                Math.sin(difflat / 2) * Math.sin(difflat / 2) +
+                    Math.cos(rlat1) *
+                        Math.cos(rlat2) *
+                        Math.sin(difflon / 2) *
+                        Math.sin(difflon / 2)
+            )
+        );
+    return d;
+}
+// ********** END OF FUNCTIONALITY 3&4.
+
+function getNearestBranches(outletsJSON) {
+    // .slice(0) to copy array by value, leaving 'outlets' unaltered.
+    let tempOutlets = outletsJSON.slice(0);
+
+    tempOutlets.sort(function (x, y) {
+        return x.distance - y.distance;
+    });
+
+    return tempOutlets.slice(0, 5);
+}
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, (error) => {
-  if (error) {
-    console.log('Something went wrong', error);
-  } else {
-    console.log('Server is listening on port ' + PORT);
-  }
+    if (error) {
+        console.log("Something went wrong", error);
+    } else {
+        console.log("Server is listening on port " + PORT);
+    }
 });
